71b56f3fcf217a05e40caf3db5e745af07f33811
Nagaraj <48904005+dnagraj@users.noreply.github.com>
2019-04-01 16:45:40 +0530

    Create comparison.sh

---
+++ comparison.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+read -p number X
+read -p number Y
+
+if [ "$X" -gt "$Y" ];
+then
+echo "X is greater than Y"
+
+elif  [ "$X" -lt "$Y" ];
+then
+echo "X is less than Y"
+
+else
+echo "X is equal to Y"
+fi
